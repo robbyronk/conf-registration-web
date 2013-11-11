1ddcd209c710f2db955103d3cc1fae8de1876151
Adam Meyer <adam.meyer@cru.org>
2013-11-11 11:17:06 -0500

    clean up

--- app/scripts/controllers/userProfile.js
+++ app/scripts/controllers/userProfile.js
@@ -2,8 +2,6 @@
 
 angular.module('confRegistrationWebApp')
   .controller('ProfileCtrl', function ($scope, $modal, $location, $http) {
-    console.log('hi');
-
     $http.get('profile/').success(function (result) {
       console.log(result);
     });
